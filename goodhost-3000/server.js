025524f88ae02e4893e4e43fdba4498782e85ab7
111xxxmoskovskijxxx111 <114381555+111xxxmoskovskijxxx111@users.noreply.github.com>
2026-03-05 01:10:14 +0200

    server.js

--- goodhost-3000/server.js
+++ goodhost-3000/server.js
@@ -4,18 +4,18 @@ const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
-// читаємо config
+
 const config = JSON.parse(fs.readFileSync("config.json"));
 const version = fs.readFileSync("version.txt", "utf-8");
 
 console.log(`[System] Starting ${config.appName} v${version}...`);
 
-// MODE 1 (буде використовуватись пізніше)
+
 if (config.mode === "mode1") {
     app.use(cors());
 }
 
-// email data
+
 const emails = [
     {
         sender: "admin@company.com",
@@ -37,4 +37,5 @@ app.get("/emails", (req, res) => {
 
 app.listen(PORT, () => {
     console.log("GoodHost running on http://localhost:3000");
+
 });
